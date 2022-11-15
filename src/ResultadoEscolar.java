3027e4f93dfdfc06086dc4b03b673d0460fe0e5c
Manelccc <emanuelmorais2015@gmail.com>
2022-11-15 14:43:28 -0300

    Organizando o repositorio

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite a nota do aluno");
+
+        int nota = scan.nextInt();
+
+        if (nota >= 7){
+        System.out.println("Aprovado");
+        }
+        else System.out.println("Reprovado");
+
+    }
+}
