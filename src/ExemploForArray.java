3b0b961acad8bb177ac8c672aba708f0a0a9f642
Manelccc <emanuelmorais2015@gmail.com>
2022-11-15 15:22:38 -0300

    Adicionando no reposytorio For em arrays

---
+++ src/ExemploForArray.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        String[] alunos = {"FELIPE", "JONAS", "JULIA","MARCOS "};
+
+
+        for (String aluno : alunos) {
+        System.out.println("O nome do aluno é: " + aluno);
+
+        }
+
+//        for (int x = 0;x < alunos.length; x++){
+//            System.out.println("O aluno no indice x = "+ x + "é"+ alunos[x]);
+//        }
+    }
+
+
+
+}
