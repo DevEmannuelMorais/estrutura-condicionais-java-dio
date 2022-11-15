983d45a09ddce407cd883c94dc84387ec39ce965
Manelccc <emanuelmorais2015@gmail.com>
2022-11-15 01:37:30 -0300

    Adicionando no reposytorio

---
+++ src/SistemaMedida.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SistemaMedida {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Escreva P,M,G");
+        String sigla = scan.next();
+//        if (sigla == "P")
+//            System.out.println("PEQUENO");
+//        else if (sigla == "M")
+//            System.out.println("MEDIO");
+//        else if (sigla == "G")
+//            System.out.println("GRANDE");
+//        else
+//            System.out.println("INDEFINIDO");
+        switch (sigla){
+            case "P":{
+                System.out.println("PEQUENO");
+                break;
+            }
+            case "M":{
+                System.out.println("MEDIO");
+                break;
+            }
+            case "G":{
+                System.out.println("GRANDE");
+                break;
+            }
+            default:
+                System.out.println("INDEFINIDO");
+        }
+
+    }
+
+}
